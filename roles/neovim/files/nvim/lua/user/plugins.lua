645874a704ca1592a798620bedb7848a1d426721
Dorin Andrei Dragan <dorin.andrei.dragan@gmail.com>
2023-12-18 21:25:07 +0200

    Fix the packer plugin install for neovim

--- roles/neovim/files/nvim/lua/user/plugins.lua
+++ roles/neovim/files/nvim/lua/user/plugins.lua
@@ -1,7 +1,7 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data')..'/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   print("Installing packer close and reopen Neovim...")
